5140904b1b07083058b01450d4e8a1870604c5ef
Misiek <misiekt@agh.edu.pl>
2019-07-17 07:13:29 +0200

    Fix company

--- d40.py
+++ d40.py
@@ -68,7 +68,7 @@ class Company:
     def add_task(self, task: str) -> None:
         self.tasks.append(task)
     def give_tasks(self, number: int):
-        self.workers.sort(key=lambda: x,x.task_count)
+        #self.workers.sort(key=lambda: x,x.task_count)
         for _, w, t in zip(range(number), self.workers, self.tasks):
             w.add_task(t)
 
@@ -80,6 +80,7 @@ class Company:
         return len(self.workers)
     def welcome(self) -> str:
         return f'Welcome all workers\nTo {self.name} company.'
+    
 
 if __name__=='__main__':
     from pprint import pprint
@@ -99,10 +100,12 @@ if __name__=='__main__':
     for _ in range(10): company.add_task(get_task())
     for _ in range(5): company.add_worker(*get_worker(), choice('SH'))
     
-    pprint(company.workers)
+
     print(f'Monthly total {company.total_salary}')
     print(company.worker_count)
     print(company.welcome())
 
     for _ in range(10): company.give_tasks(10)
     
+    pprint(company.workers)
+    pprint([list(t.tasks) for t in company.workers])
